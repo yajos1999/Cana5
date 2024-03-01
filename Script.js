5d213cb98fe4cc24bad6433d6123ea002b55aaca
yajos1999 <161801972+yajos1999@users.noreply.github.com>
2024-03-01 00:25:19 -0600

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,6 +1,12 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var player = videojs('my-video');
-  player.ready(function() {
-    player.play();
-  });
+jwplayer('playerAJxJyoaBJaaN').setup({
+    file: 'https://linear-351.frequency.stream/mt/studio/351/hls/master/playlist.m3u8',
+    title: 'CANAL 5 GRATIS',
+    width: '100%',
+    aspectratio: '16:9',
+    mute: 'true',
+    autostart: 'true',
+    hlshtml: true,
+    preload: 'metadata', // Cambiamos de 'auto' a 'metadata' para cargar solo los metadatos del vídeo inicialmente
+    androidhls: true,
+    hlsjs: true
 });
